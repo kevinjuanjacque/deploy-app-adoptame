be5f50843b09d6cd69454be3efa560dda2a17885
Kevin Juan Jacque Castillo <kjjacquec@falabella.cl>
2023-06-07 21:18:31 -0400

    feat added views

--- src/app.js
+++ src/app.js
@@ -6,14 +6,28 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import router from './routes/views.router.js';
+import { engine } from 'express-handlebars';
+import __dirname from './utils/index.js';
+import path from 'path'
 
 const app = express();
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 8080;
 const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
 
+
+app.use(express.static('public'))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'..','views'));
+
+
+
+app.use('/',router)
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
